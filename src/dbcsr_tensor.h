6a80382026acb8d5b3a94af7220447b3217665e6
ambmax00 <maximilien.ambroise@gmail.com>
2019-11-18 14:01:18 +0100

    Continue in API: iterator subroutines.

--- src/dbcsr_tensor.h
+++ src/dbcsr_tensor.h
@@ -12,8 +12,7 @@ extern "C" {
 #endif
     void c_dbcsr_t_pgrid_create(MPI_Fint* fcomm, int* c_dims, int dims_size, void** c_pgrid, 
 		int* c_map1_2d, int map1_2d_size, int* c_map2_2d, int map2_2d_size, int* nsplit, int* dimsplit);
-																			   //^            ^
-																			   // needed by reference, to check if present 
+																			  
 	void c_dbcsr_t_distribution_new(void** c_dist, void* c_pgrid, int* c_map1_2d, int map1_2d_size,
 		int* c_map2_2d, int map2_2d_size, ${extern_varlist_and_size("c_nd_dist")}$, bool* own_comm);
 		
@@ -40,24 +39,61 @@ extern "C" {
     int c_ndims_tensor(void* tensor);
                                
 #:for dsuffix, ctype in c_dtype_float_list
-#:for ndim in ndims    
+#:for ndim in ndims
+
+	  void c_dbcsr_t_allocate_and_get_${ndim}$d_block_${dsuffix}$ (void* c_tensor, int tensor_dim, int* c_ind, 
+		${ctype}$** c_block, bool* c_found);
                     
       void c_dbcsr_t_get_${ndim}$d_block_${dsuffix}$ (void* c_tensor, int tensor_dim, int* c_ind, int* c_sizes, 
-		${ctype}$** c_block, bool* c_found);
+		${ctype}$* c_block, bool* c_found);
 		
 	  void c_dbcsr_t_put_${ndim}$d_block_${dsuffix}$ (void* c_tensor, int tensor_dim, int* c_ind, int*c_sizes, 
         ${ctype}$* c_block, bool* c_summation, ${ctype}$* c_scale);
 	
 #:endfor
-#:endfor      	
+#:endfor  
+
+     void c_dbcsr_t_get_stored_coordinates(void* c_tensor, int tensor_dim, int* c_ind_nd, int* c_processor);
+   
+     void c_dbcsr_t_reserve_blocks_index(void* c_tensor, ${extern_varlist_and_size("c_blk_ind")}$);
+     
+     int c_ndims_iterator(void* c_iterator); 
+     
+	 void c_dbcsr_t_iterator_start(void** c_iterator, void* c_tensor);
+
+     void c_dbcsr_t_iterator_stop(void* c_iterator);
+   
+     void c_dbcsr_t_iterator_next_block(void* c_iterator, int iterator_size, int* c_ind_nd, 
+          int* c_blk, int* c_blk_p, int* c_blk_size, int* c_blk_offset);
+          
+     bool c_dbcsr_t_iterator_blocks_left(void* c_iterator);
+     
+ /*    void c_dbcsr_t_get_info(void* c_tensor, int tensor_dim, int* c_nblks_total,
+                               int* c_nfull_total,
+                               int* c_nblks_local, 
+                               int* c_nfull_local, 
+                               int* c_pdims, 
+                               int* c_my_ploc, 
+                               ${extern_alloc_varlist_and_size("c_blks_local")}$, 
+                               ${extern_alloc_varlist_and_size("c_proc_dist")}$, 
+                               ${extern_alloc_varlist_and_size("c_blk_size")}$, 
+                               ${extern_alloc_varlist_and_size("c_blk_offset")}$, 
+                               void** c_distribution, 
+                               const char* c_name, 
+                               int* c_data_type);*/
+         
 	
 #ifdef __cplusplus
 }
 #endif
 
+/* ***************************************************************
+ * ********* OVERLOADED C FUNCTIONS ******************************
+ * ***************************************************************/
+
 #:for dsuffix, ctype in c_dtype_float_list
 static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, int* c_sizes, 
-		${ctype}$** c_block, bool* c_found) {
+		${ctype}$* c_block, bool* c_found) {
 	
 	int tensor_dim = c_ndims_tensor(c_tensor);
 	
@@ -65,6 +101,22 @@ static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, int* c_sizes,
 		#:for ndim in ndims
 		case ${ndim}$: c_dbcsr_t_get_${ndim}$d_block_${dsuffix}$ (c_tensor, tensor_dim, 
 		c_ind, c_sizes, c_block, c_found);
+		#:endfor
+	}
+	
+}
+#:endfor
+
+#:for dsuffix, ctype in c_dtype_float_list
+static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, 
+		${ctype}$** c_block, bool* c_found) {
+	
+	int tensor_dim = c_ndims_tensor(c_tensor);
+	
+	switch(tensor_dim) {
+		#:for ndim in ndims
+		case ${ndim}$: c_dbcsr_t_allocate_and_get_${ndim}$d_block_${dsuffix}$ (c_tensor, tensor_dim, 
+		c_ind, c_block, c_found);
 		#:endfor
 	}
 	
@@ -86,5 +138,55 @@ static void c_dbcsr_t_put_block(void* c_tensor, int* c_ind, int* c_sizes,
 	
 }
 #:endfor
+
+static void c_dbcsr_t_get_stored_coordinates(void* c_tensor, int* c_ind_nd, int* c_processor) {
+	
+	int tensor_dim = c_ndims_tensor(c_tensor);
+	c_dbcsr_t_get_stored_coordinates(c_tensor, tensor_dim, c_ind_nd, c_processor);
+	
+}
+
+static void c_dbcsr_t_iterator_next_block(void* c_iterator, int* c_ind_nd, 
+          int* c_blk, int* c_blk_p, int* c_blk_size, int* c_blk_offset) {
+			  
+	int iterator_size = c_ndims_iterator(c_iterator);
+	
+	c_dbcsr_t_iterator_next_block(c_iterator, iterator_size, c_ind_nd, 
+          c_blk, c_blk_p, c_blk_size, c_blk_offset);
+          
+}
+
+/*
+static void c_dbcsr_t_get_info(void* c_tensor, int* c_nblks_total,
+                               int* c_nfull_total,
+                               int* c_nblks_local, 
+                               int* c_nfull_local, 
+                               int* c_pdims, 
+                               int* c_my_ploc, 
+                               ${extern_alloc_varlist_and_size("c_blks_local")}$, 
+                               ${extern_alloc_varlist_and_size("c_proc_dist")}$, 
+                               ${extern_alloc_varlist_and_size("c_blk_size")}$, 
+                               ${extern_alloc_varlist_and_size("c_blk_offset")}$, 
+                               void** c_distribution, 
+                               const char* c_name, 
+                               int* c_data_type) {
+								   
+		int tensor_dim = c_ndims_tensor(c_tensor);	
+		c_dbcsr_t_get_info(void* c_tensor, int tensor_dim, int* c_nblks_total,
+                               int* c_nfull_total,
+                               int* c_nblks_local, 
+                               int* c_nfull_local, 
+                               int* c_pdims, 
+                               int* c_my_ploc, 
+                               ${extern_alloc_varlist_and_size("c_blks_local")}$, 
+                               ${extern_alloc_varlist_and_size("c_proc_dist")}$, 
+                               ${extern_alloc_varlist_and_size("c_blk_size")}$, 
+                               ${extern_alloc_varlist_and_size("c_blk_offset")}$, 
+                               void** c_distribution, 
+                               const char* c_name, 
+                               int* c_data_type);
+                               
+}			
+*/	
 
 #endif // DBCSR_H

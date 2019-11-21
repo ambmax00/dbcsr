4cdd8ed2e67184037a1d44d9237424322962cb1c
ambmax00 <maximilien.ambroise@gmail.com>
2019-11-21 15:48:47 +0100

    C interface: Fix 0/1 index bugs. Fix contract tensor.

--- src/dbcsr_tensor.h
+++ src/dbcsr_tensor.h
@@ -6,6 +6,7 @@
 
 #:include "tensors/dbcsr_tensor.fypp"
 #:set ndims = range(2,maxrank+1)
+#:set ddims = range(1,maxrank+1)
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,34 +14,17 @@ extern "C" {
     void c_dbcsr_t_pgrid_create(MPI_Fint* fcomm, int* c_dims, int dims_size, void** c_pgrid, 
 		int* c_map1_2d, int map1_2d_size, int* c_map2_2d, int map2_2d_size, int* nsplit, int* dimsplit);
 	
-	void c_dbcsr_t_pgrid_destroy(void* c_pgrid, bool* c_keep_comm);
+	void c_dbcsr_t_pgrid_destroy(void** c_pgrid, bool* c_keep_comm);
 																			  
 	void c_dbcsr_t_distribution_new(void** c_dist, void* c_pgrid, int* c_map1_2d, int map1_2d_size,
 		int* c_map2_2d, int map2_2d_size, ${extern_varlist_and_size("c_nd_dist")}$, bool* own_comm);
 		
-    void c_dbcsr_t_distribution_destroy(void* c_dist);
+    void c_dbcsr_t_distribution_destroy(void** c_dist);
 		
 	void c_dbcsr_t_create_new(void** c_tensor, const char* c_name, void* c_dist, int* c_map1_2d, int c_map1_2d_size,
        int* c_map2_2d, int c_map2_2d_size, int* data_type, ${extern_varlist_and_size("c_blk_size")}$);
        
-    void c_dbcsr_t_destroy(void* c_tensor);
-
-	void c_dbcsr_t_contract(int* c_alpha, void* c_tensor_1, void* c_tensor_2, int* c_beta, void* c_tensor_3, 
-                               int* c_contract_1, int c_contract_1_size,
-                               int* c_notcontract_1, int c_notcontract_1_size,
-                               int* c_contract_2, int c_contract_2_size,
-                               int* c_notcontract_2, int c_notcontract_2_size,
-                               int* c_map_1, int c_map_1_size,
-                               int* c_map_2, int c_map_2_size,
-                               int* c_bounds_1, int c_bounds_1_size,
-                               int* c_bounds_2, int c_bounds_2_size,
-                               int* c_bounds_3, int c_bounds_3_size,
-                               bool* c_optimize_dist, 
-                               void** c_pgrid_opt_1, 
-                               void** c_pgrid_opt_2, 
-                               void** c_pgrid_opt_3,
-                               double* filter_eps, long long int* flop, 
-                               int* move_data, int* unit_nr, bool* log_verbose);
+    void c_dbcsr_t_destroy(void** c_tensor);
                                
     int c_ndims_tensor(void* tensor);
                                
@@ -57,6 +41,23 @@ extern "C" {
         ${ctype}$* c_block, bool* c_summation, ${ctype}$* c_scale);
 
 #:endfor
+     
+     void c_dbcsr_t_contract_${dsuffix}$ (${ctype}$ c_alpha, void* c_tensor_1, void* c_tensor_2, ${ctype}$ c_beta, void* c_tensor_3, 
+                               int* c_contract_1, int c_contract_1_size,
+                               int* c_notcontract_1, int c_notcontract_1_size,
+                               int* c_contract_2, int c_contract_2_size,
+                               int* c_notcontract_2, int c_notcontract_2_size,
+                               int* c_map_1, int c_map_1_size,
+                               int* c_map_2, int c_map_2_size,
+                               int* c_bounds_1, int c_bounds_1_size,
+                               int* c_bounds_2, int c_bounds_2_size,
+                               int* c_bounds_3, int c_bounds_3_size,
+                               bool* c_optimize_dist, 
+                               void** c_pgrid_opt_1, 
+                               void** c_pgrid_opt_2, 
+                               void** c_pgrid_opt_3,
+                               double* filter_eps, long long int* flop, 
+                               int* move_data, int* unit_nr, bool* log_verbose);
      
      void c_dbcsr_t_filter_${dsuffix}$ (void* c_tensor, ${ctype}$ c_eps, int* c_method, bool* c_use_absolute);
      
@@ -75,7 +76,7 @@ extern "C" {
      
 	 void c_dbcsr_t_iterator_start(void** c_iterator, void* c_tensor);
 
-     void c_dbcsr_t_iterator_stop(void* c_iterator);
+     void c_dbcsr_t_iterator_stop(void** c_iterator);
    
      void c_dbcsr_t_iterator_next_block(void* c_iterator, int iterator_size, int* c_ind_nd, 
           int* c_blk, int* c_blk_p, int* c_blk_size, int* c_blk_offset);
@@ -114,9 +115,11 @@ extern "C" {
 }
 #endif
 
-/* ***************************************************************
- * ********* OVERLOADED C FUNCTIONS ******************************
- * ***************************************************************/
+/* *************************************************************************************************
+ * *                                                                                               *
+ * *                                      OVERLOADED C FUNCTIONS                                   *
+ * *                                         *                                                     *
+ * *************************************************************************************************/
 
 #:for dsuffix, ctype in c_dtype_float_list
 static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, int* c_sizes, 
@@ -128,6 +131,7 @@ static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, int* c_sizes,
 		#:for ndim in ndims
 		case ${ndim}$: c_dbcsr_t_get_${ndim}$d_block_${dsuffix}$ (c_tensor, tensor_dim, 
 		c_ind, c_sizes, c_block, c_found);
+		break;
 		#:endfor
 	}
 	
@@ -144,6 +148,7 @@ static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind,
 		#:for ndim in ndims
 		case ${ndim}$: c_dbcsr_t_allocate_and_get_${ndim}$d_block_${dsuffix}$ (c_tensor, tensor_dim, 
 		c_ind, c_block, c_found);
+		break;
 		#:endfor
 	}
 	
@@ -160,6 +165,7 @@ static void c_dbcsr_t_put_block(void* c_tensor, int* c_ind, int* c_sizes,
 		#:for ndim in ndims
 		case ${ndim}$: c_dbcsr_t_put_${ndim}$d_block_${dsuffix}$ (c_tensor, tensor_dim, 
 		c_ind, c_sizes, c_block, c_summation, c_scale);
+		break;
 		#:endfor
 	}
 	
@@ -172,6 +178,21 @@ static void c_dbcsr_t_get_stored_coordinates(void* c_tensor, int* c_ind_nd, int*
 	c_dbcsr_t_get_stored_coordinates(c_tensor, tensor_dim, c_ind_nd, c_processor);
 	
 }
+
+
+#:for ndim in range(2, maxrank)
+static void c_dbcsr_t_reserve_blocks_index(void* c_tensor, ${extern_varlist_and_size("c_blk_ind", nmax=ndim)}$) {
+	
+	c_dbcsr_t_reserve_blocks_index(c_tensor, ${c_varlist_and_size("c_blk_ind", nmax=ndim)}$, 
+#:for n in range(ndim+1,maxrank+1)
+  #:if n == maxrank
+    nullptr, 0);
+  #:else
+    nullptr, 0,
+  #:endif
+#:endfor
+};
+#:endfor
 
 static void c_dbcsr_t_iterator_next_block(void* c_iterator, int* c_ind_nd, 
           int* c_blk, int* c_blk_p, int* c_blk_size, int* c_blk_offset) {
@@ -210,6 +231,43 @@ static void* c_dbcsr_t_blk_sizes(void* c_tensor, int* c_ind, int* c_blk_size) {
 	c_dbcsr_t_blk_sizes(c_tensor, tensor_dim, c_ind, c_blk_size);
 	
 }
+
+/*
+static void c_dbcsr_t_contract (${ctype}$ c_alpha, void* c_tensor_1, void* c_tensor_2, ${ctype}$ c_beta, void* c_tensor_3, 
+                               int* c_contract_1, int c_contract_1_size,
+                               int* c_notcontract_1, int c_notcontract_1_size,
+                               int* c_contract_2, int c_contract_2_size,
+                               int* c_notcontract_2, int c_notcontract_2_size,
+                               int* c_map_1, int c_map_1_size,
+                               int* c_map_2, int c_map_2_size,
+                               int* c_bounds_1, int c_bounds_1_size,
+                               int* c_bounds_2, int c_bounds_2_size,
+                               int* c_bounds_3, int c_bounds_3_size,
+                               bool* c_optimize_dist, 
+                               void** c_pgrid_opt_1, 
+                               void** c_pgrid_opt_2, 
+                               void** c_pgrid_opt_3,
+                               double* filter_eps, long long int* flop, 
+                               int* move_data, int* unit_nr, bool* log_verbose) {
+								  
+	c_dbcsr_t_contract_${dsuffix}$ (c_alpha, c_tensor_1, c_tensor_2, c_beta, c_tensor_3, 
+                               ic_contract_1, c_contract_1_size,
+                               c_notcontract_1, c_notcontract_1_size,
+                               c_contract_2, c_contract_2_size,
+                               c_notcontract_2, c_notcontract_2_size,
+                               c_map_1, c_map_1_size,
+                               c_map_2, c_map_2_size,
+                               c_bounds_1, c_bounds_1_size,
+                               c_bounds_2, c_bounds_2_size,
+                               c_bounds_3, c_bounds_3_size,
+                               c_optimize_dist, 
+                               c_pgrid_opt_1, 
+                               c_pgrid_opt_2, 
+                               c_pgrid_opt_3,
+                               filter_eps, flop, 
+                               move_data, unit_nr, log_verbose);
+                              
+}*/
 
 
 #endif // DBCSR_H

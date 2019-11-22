bdec59b37fd46a347f36116bd4a55f33ba9babbe
ambmax00 <maximilien.ambroise@gmail.com>
2019-11-22 16:50:35 +0100

    Remove comment.

--- cmake/fypp-sources.cmake
+++ cmake/fypp-sources.cmake
@@ -25,13 +25,11 @@ function (ADD_FYPP_SOURCES OUTVAR)
     endif ()
 
     if ("${f}" MATCHES ".F$")
-      message(STATUS "Processing a fotran thing...")	    
       # append the output file to the list of outputs
       list(APPEND outfiles "${of}")
       # now add the custom command to generate the output file
       add_custom_command(OUTPUT "${of}" COMMAND ${FYPP_EXECUTABLE} ARGS "${f}" "${of}" DEPENDS "${f}")
     elseif("${f}" MATCHES ".h$")
-      message(STATUS "Processing header...")
      # append the output file to the list of outputs
       list(APPEND outfiles "${of}")
       # now add the custom command to generate the output file
